4d88b8a6e1033495541a00e8d53855ba3f148b9c
Andrea Tamburini <attheapplab@gmail.com>
2021-07-26 23:19:15 +0200

    Mon Jul 26 23:19:15 CEST 2021

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
@@ -10,4 +10,4 @@ module.exports = merge(common, {
     host: '0.0.0.0',
     writeToDisk: true,
   },
-});
+})
